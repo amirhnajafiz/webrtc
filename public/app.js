3f2f7bd236c92643068a75f4f66da6aef9a06bb6
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-28 10:22:52 +0330

    set listener for calle call adding

--- public/app.js
+++ public/app.js
@@ -52,13 +52,22 @@ async function createRoom() {
 
   document.querySelector('#currentRoom').innerText = `Current room is ${roomId} - You are the caller!`;
   
-  
+
   localStream.getTracks().forEach(track => {
     peerConnection.addTrack(track, localStream);
   });
 
   // Code for creating a room below
+  roomRef.onSnapshot(async snapshot => {
+    console.log('Got updated room:', snapshot.data());
+    const data = snapshot.data();
 
+    if (!peerConnection.currentRemoteDescription && data.answer) {
+      console.log('Set remote description: ', data.answer);
+      const answer = new RTCSessionDescription(data.answer);
+      await peerConnection.setRemoteDescription(answer);
+    }
+  });
   // Code for creating a room above
 
   // Code for collecting ICE candidates below
