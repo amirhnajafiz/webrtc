ad257d92ba9c75b129b8f29e98a8b3746b0154bb
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-10 18:55:06 +0330

    update: main handler

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/amirhnajafiz/webrtc/internal/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	httpHandler := http.Handler{
+		Connections: make([]*websocket.Conn, 0),
+	}
+
+	app.Static("/static/", "./web/static")
+
+	app.Get("/ws", websocket.New(httpHandler.WebsocketHandler))
+
+	if err := app.Listen(":8080"); err != nil {
+		log.Println(fmt.Errorf("failed to start signaling server"))
+	}
 }
